8964e1c6a09eb9f4918d3df7fad06931882125ea
Jitendra42Repo <72111501+Jitendra42Repo@users.noreply.github.com>
2020-11-22 23:30:44 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyC4swZ72S0Y4RO3P2zVj_Meef7h1nfRFBs",
-    authDomain: "vmart-9f3fd.firebaseapp.com",
-    databaseURL: "https://vmart-9f3fd.firebaseio.com",
-    projectId: "vmart-9f3fd",
-    storageBucket: "vmart-9f3fd.appspot.com",
-    messagingSenderId: "298885974630",
-    appId: "1:298885974630:web:1274eb3efa23a9275efba7"
+    apiKey: "********************",
+    authDomain: "***-**.firebaseapp.com",
+    databaseURL: "https://vmart-******.****.com",
+    projectId: "*******",
+    storageBucket: "*********",
+    messagingSenderId: "********",
+    appId: "*************"
   }
 };
 
